5c9ba67ee82ec99176a80b4c1a41e96ecc331d8f
Michael Joyner <mooz@Michaels-MacBook-Pro.local>
2015-05-08 09:54:03 +0800

    homepage responsive

--- resources/views/front/partials/courses.blade.php
+++ resources/views/front/partials/courses.blade.php
@@ -3,29 +3,29 @@
     <p>Our services Lorem ipsum dolor sit amet, consectetur adipisicing elit. At atque dignissimos doloremque. Debitis, delectus doloribus eaque eveniet excepturi fugiat, in iste magni mollitia nam necessitatibus nostrum officiis placeat quis ut?</p>
 
     <div class="row">
-        <div class="col-md-4 course-box">
+        <div class="col-md-4 col-sm-4 course-box">
             <h1 class="item-name">course name</h1>
             <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, aut beatae cupiditate debitis deserunt et eveniet excepturi exercitationem, inventore molestiae nulla officia pariatur provident repellendus sit sunt tenetur totam ut.</p>
         </div>
-        <div class="col-md-4 course-box">
+        <div class="col-md-4 col-sm-4 course-box">
             <h1 class="item-name">course name</h1>
             <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, aut beatae cupiditate debitis deserunt et eveniet excepturi exercitationem, inventore molestiae nulla officia pariatur provident repellendus sit sunt tenetur totam ut.</p>
         </div>
-        <div class="col-md-4 course-box">
+        <div class="col-md-4 col-sm-4 course-box">
             <h1 class="item-name">course name</h1>
             <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, aut beatae cupiditate debitis deserunt et eveniet excepturi exercitationem, inventore molestiae nulla officia pariatur provident repellendus sit sunt tenetur totam ut.</p>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-4 course-box">
+        <div class="col-md-4 col-sm-4 course-box">
             <h1 class="item-name">course name</h1>
             <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, aut beatae cupiditate debitis deserunt et eveniet excepturi exercitationem, inventore molestiae nulla officia pariatur provident repellendus sit sunt tenetur totam ut.</p>
         </div>
-        <div class="col-md-4 course-box">
+        <div class="col-md-4 col-sm-4 course-box">
             <h1 class="item-name">course name</h1>
             <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, aut beatae cupiditate debitis deserunt et eveniet excepturi exercitationem, inventore molestiae nulla officia pariatur provident repellendus sit sunt tenetur totam ut.</p>
         </div>
-        <div class="col-md-4 course-box">
+        <div class="col-md-4 col-sm-4 course-box">
             <h1 class="item-name">course name</h1>
             <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, aut beatae cupiditate debitis deserunt et eveniet excepturi exercitationem, inventore molestiae nulla officia pariatur provident repellendus sit sunt tenetur totam ut.</p>
         </div>
