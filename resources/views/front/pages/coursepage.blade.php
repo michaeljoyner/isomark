537d24924a1ff2790e39d2bfbaf5c95094f6c25c
Michael Joyner <joyner.michael@gmail.com>
2015-05-08 10:42:50 +0800

    services and courses responsive

--- resources/views/front/pages/coursepage.blade.php
+++ resources/views/front/pages/coursepage.blade.php
@@ -11,9 +11,9 @@
     <div class="container">
         <p class="courses-intro"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis delectus distinctio eum exercitationem facere minima omnis perspiciatis soluta vel voluptatem? Animi eligendi est labore laborum magnam nam placeat quasi sint.</span><span>Ab ad aliquam beatae consequatur exercitationem facere modi molestias, necessitatibus nemo nulla quod sequi temporibus veniam? Dolore esse iure labore nemo nostrum quia. A beatae itaque numquam repudiandae suscipit tempora?</span><span>Accusamus adipisci asperiores aspernatur enim exercitationem, iure neque nesciunt non omnis optio praesentium quaerat, saepe, sed sequi sunt tempora vero. Ad excepturi fuga modi officia omnis qui rem unde vitae.</span>
         </p>
-        <div class="course-icons-container clearfix">
-            <div class="course-icon-row clearfix">
-                <div class="pull-left clearfix">
+        <div class="course-icons-container">
+            <div class="course-icon-row row">
+                <div class="col-sm-6">
                     <a href="/courses/safety-health-and-environment">
                         <div class="course-icon-box">
                             @include('svg.isomark_website_icons-13')
@@ -21,26 +21,42 @@
                         </div>
                     </a>
                 </div>
-                <div class="pull-right clearfix"><a href="/courses/lifting-equipment-training"><div class="course-icon-box">
-                    @include('svg.isomark_website_icons-14')
-                    <h4>Lifting Equipment</h4>
-                </div></a></div>
+                <div class="col-sm-6">
+                    <a href="/courses/lifting-equipment-training">
+                        <div class="course-icon-box">
+                            @include('svg.isomark_website_icons-14')
+                            <h4>Lifting Equipment</h4>
+                        </div>
+                    </a>
+                </div>
             </div>
-            <div class="course-icon-row clearfix">
-                <div class="pull-left"><a href="/courses/soft-skills-programmes"><div class="course-icon-box">
-                    @include('svg.isomark_website_icons-15')
-                    <h4>Soft Skills Programmes</h4>
-                </div></a></div>
-                <div class="pull-right"><a href="/courses/skills-bundles"><div class="course-icon-box">
-                    @include('svg.isomark_website_icons-16')
-                    <h4>Skills Bundles</h4>
-                </div></a></div>
+            <div class="course-icon-row row">
+                <div class="col-sm-6">
+                    <a href="/courses/soft-skills-programmes">
+                        <div class="course-icon-box">
+                            @include('svg.isomark_website_icons-15')
+                            <h4>Soft Skills Programmes</h4>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm-6">
+                    <a href="/courses/skills-bundles">
+                        <div class="course-icon-box">
+                            @include('svg.isomark_website_icons-16')
+                            <h4>Skills Bundles</h4>
+                        </div>
+                    </a>
+                </div>
             </div>
-            <div class="course-icon-row clearfix">
-                <div class="pull-left"><a href="/workshops"><div class="course-icon-box">
-                    @include('svg.isomark_website-18')
-                    <h4>Workshops</h4>
-                </div></a></div>
+            <div class="course-icon-row row">
+                <div class="col-sm-6">
+                    <a href="/workshops">
+                        <div class="course-icon-box">
+                            @include('svg.isomark_website-18')
+                            <h4>Workshops</h4>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/front/pages/workshops.blade.php
+++ resources/views/front/pages/workshops.blade.php
@@ -10,30 +10,30 @@
     </header>
     <div class="container workshop-container">
         <div class="row workshop-list-head workshop-row">
-            <div class="col-md-6 workshop-detail">
+            <div class="col-sm-6 workshop-detail">
                 Workshop
             </div>
-            <div class="col-md-2 workshop-detail">
+            <div class="col-sm-2 workshop-detail">
                 Duration
             </div>
-            <div class="col-md-2 workshop-detail">
+            <div class="col-sm-2 workshop-detail">
                 Fee
             </div>
-            <div class="col-md-2 workshop-detail">
+            <div class="col-sm-2 workshop-detail">
             </div>
         </div>
         @foreach($workshops as $workshop)
             <div class="row workshop-row">
-                <div class="col-md-6 workshop-detail">
+                <div class="col-sm-6 workshop-detail">
                     {{ $workshop->name }}
                 </div>
-                <div class="col-md-2 workshop-detail">
+                <div class="col-sm-2 workshop-detail">
                     {{ $workshop->duration }}
                 </div>
-                <div class="col-md-2 workshop-detail">
+                <div class="col-sm-2 workshop-detail">
                     {{ $workshop->fee }}
                 </div>
-                <div class="col-md-2 workshop-detail clearfix">
+                <div class="col-sm-2 workshop-detail clearfix">
                     <a href="/workshops/bookings/{{ $workshop->slug }}" class="btn prompt-button btn-xs pull-right">Make Booking</a>
                 </div>
             </div>
