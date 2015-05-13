085894715f21d35e16f3f6dbcede7a39f4e16d13
Michael Joyner <joyner.michael@gmail.com>
2015-05-13 11:17:27 +0800

    make carousel more responsive

--- resources/views/front/partials/carousel.blade.php
+++ resources/views/front/partials/carousel.blade.php
@@ -1,40 +1,52 @@
 <section>
-<div id="slides">
-    <div class="carousel-slide clearfix">
-        <img src="{{ asset('images/carousel/Firefighting.jpg') }}" alt=""/>
-        <div class="slide-text-holder">
-            <p>Fire Fighting</p>
+    <div id="slides">
+        <div class="carousel-slide clearfix">
+            <img src="{{ asset('images/carousel/SHErep.jpg') }}" alt=""/>
+
+            <div class="slide-text-holder">
+                <p>SHE Representative</p>
+                <p class="subtext">Training Course Available</p>
+            </div>
+        </div>
+        <div class="carousel-slide clearfix">
+            <img src="{{ asset('images/carousel/Firefighting.jpg') }}" alt=""/>
+
+            <div class="slide-text-holder">
+                <p>Basic Fire Fighting</p>
+                <p class="subtext">Training Course Available</p>
+            </div>
+        </div>
+        <div class="carousel-slide clearfix">
+            <img src="{{ asset('images/carousel/HIRA.jpg') }}" alt=""/>
+
+            <div class="slide-text-holder">
+                <p>Hazardous Identification Risk Assessment (H.I.R.A)</p>
+                <p class="subtext">Training Course Available</p>
+            </div>
+        </div>
+        <div class="carousel-slide clearfix">
+            <img src="{{ asset('images/carousel/FirstAid1.jpg') }}" alt=""/>
+
+            <div class="slide-text-holder">
+                <p>First Aid Training</p>
+                <p class="subtext">Training Course Available</p>
+            </div>
+        </div>
+        <div class="carousel-slide clearfix">
+            <img src="{{ asset('images/carousel/lifting_equipment.jpg') }}" alt=""/>
+
+            <div class="slide-text-holder">
+                <p>Lifting Equipment Training</p>
+                <p class="subtext">Training Course Available</p>
+            </div>
+        </div>
+        <div class="carousel-slide clearfix">
+            <img src="{{ asset('images/carousel/incidentinvestigation.jpg') }}" alt=""/>
+
+            <div class="slide-text-holder">
+                <p>Incident Investigation</p>
+                <p class="subtext">Training Course Available</p>
+            </div>
         </div>
     </div>
-    <div class="carousel-slide clearfix">
-        <img src="{{ asset('images/carousel/hardhat.jpg') }}" alt=""/>
-        <div class="slide-text-holder">
-            <p>H.I.R.A</p>
-        </div>
-    </div>
-    <div class="carousel-slide clearfix">
-        <img src="{{ asset('images/carousel/FirstAid1.jpg') }}" alt=""/>
-        <div class="slide-text-holder">
-            <p>First Aid Training</p>
-        </div>
-    </div>
-    <div class="carousel-slide clearfix">
-        <img src="{{ asset('images/carousel/forklift_boxes.jpg') }}" alt=""/>
-        <div class="slide-text-holder">
-            <p>Lifting Equipment Training</p>
-        </div>
-    </div>
-    <div class="carousel-slide clearfix">
-        <img src="{{ asset('images/carousel/incidentinvestigation.jpg') }}" alt=""/>
-        <div class="slide-text-holder">
-            <p>Incident Investigation</p>
-        </div>
-    </div>
-    <div class="carousel-slide clearfix">
-        <img src="{{ asset('images/carousel/SHErep.jpg') }}" alt=""/>
-        <div class="slide-text-holder">
-            <p>SHE Representative</p>
-        </div>
-    </div>
-</div>
 </section>
