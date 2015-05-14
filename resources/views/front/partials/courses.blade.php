33d3aeb2d4a7aab66b72b809c6033693fdffbe59
Michael Joyner <joyner.michael@gmail.com>
2015-05-14 11:33:31 +0800

    nav arrows on carousel

--- resources/views/front/partials/courses.blade.php
+++ resources/views/front/partials/courses.blade.php
@@ -2,45 +2,16 @@
 <div class="section-content container">
     <p>Our services Lorem ipsum dolor sit amet, consectetur adipisicing elit. At atque dignissimos doloremque. Debitis, delectus doloribus eaque eveniet excepturi fugiat, in iste magni mollitia nam necessitatibus nostrum officiis placeat quis ut?</p>
 
-    <div class="row">
+    <div class="row course-row">
         @foreach($courses as $index => $course)
             <div class="col-md-4 col-sm-4 course-box">
                 <h1 class="item-name">{{ $course->name }}</h1>
                 <p class="item-description">{{ $course->aim }}</p>
             </div>
             @if($index === 2)
-                </div><div class="row">
+                </div><div class="row course-row">
             @endif
         @endforeach
     </div>
-
-    {{--<div class="row">--}}
-        {{--<div class="col-md-4 col-sm-4 course-box">--}}
-            {{--<h1 class="item-name">course name</h1>--}}
-            {{--<p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, aut beatae cupiditate debitis deserunt et eveniet excepturi exercitationem, inventore molestiae nulla officia pariatur provident repellendus sit sunt tenetur totam ut.</p>--}}
-        {{--</div>--}}
-        {{--<div class="col-md-4 col-sm-4 course-box">--}}
-            {{--<h1 class="item-name">course name</h1>--}}
-            {{--<p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, aut beatae cupiditate debitis deserunt et eveniet excepturi exercitationem, inventore molestiae nulla officia pariatur provident repellendus sit sunt tenetur totam ut.</p>--}}
-        {{--</div>--}}
-        {{--<div class="col-md-4 col-sm-4 course-box">--}}
-            {{--<h1 class="item-name">course name</h1>--}}
-            {{--<p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, aut beatae cupiditate debitis deserunt et eveniet excepturi exercitationem, inventore molestiae nulla officia pariatur provident repellendus sit sunt tenetur totam ut.</p>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-    {{--<div class="row">--}}
-        {{--<div class="col-md-4 col-sm-4 course-box">--}}
-            {{--<h1 class="item-name">course name</h1>--}}
-            {{--<p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, aut beatae cupiditate debitis deserunt et eveniet excepturi exercitationem, inventore molestiae nulla officia pariatur provident repellendus sit sunt tenetur totam ut.</p>--}}
-        {{--</div>--}}
-        {{--<div class="col-md-4 col-sm-4 course-box">--}}
-            {{--<h1 class="item-name">course name</h1>--}}
-            {{--<p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, aut beatae cupiditate debitis deserunt et eveniet excepturi exercitationem, inventore molestiae nulla officia pariatur provident repellendus sit sunt tenetur totam ut.</p>--}}
-        {{--</div>--}}
-        {{--<div class="col-md-4 col-sm-4 course-box">--}}
-            {{--<h1 class="item-name">course name</h1>--}}
-            {{--<p class="item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, aut beatae cupiditate debitis deserunt et eveniet excepturi exercitationem, inventore molestiae nulla officia pariatur provident repellendus sit sunt tenetur totam ut.</p>--}}
-        {{--</div>--}}
-    {{--</div>--}}
     <a href="/courses"><button class="btn prompt-button spaced">more courses</button></a>
 </div>
