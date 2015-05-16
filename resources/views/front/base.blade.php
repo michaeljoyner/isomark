b9d544577b10c7333b6a455198fd166c0dac7ce4
Michael Joyner <joyner.michael@gmail.com>
2015-05-16 17:58:59 +0800

    classlist polyfill for ie9

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -20,6 +20,9 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+    <!--[if IE 9]>
+        <script src="{{ asset("js/classList.min.js") }}"></script>
+    <![endif]-->
         <script>
             /* Modernizr 2.8.3 (Custom Build) | MIT & BSD
              * Build: http://modernizr.com/download/#-multiplebgs-csstransforms-inlinesvg-svg-touch-cssclasses-teststyles-testprop-testallprops-prefixes-domprefixes-load
