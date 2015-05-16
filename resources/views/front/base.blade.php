76c8190caf72afae5b9eb17b044cf4d829b2fb6c
Michael Joyner <joyner.michael@gmail.com>
2015-05-16 18:35:28 +0800

    hide slide nav in ie9

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -19,6 +19,11 @@
 	<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <style>
+            .slide-nav-menu {
+                display: none;
+            }
+        </style>
 	<![endif]-->
     <!--[if IE 9]>
         <script src="{{ asset("js/classList.min.js") }}"></script>
