268cc0f20b4adebbfb4dc264d8d25b1c9c5a82e2
Michael Joyner <joyner.michael@gmail.com>
2015-05-19 21:55:47 +0800

    link hp courses to actual course page, no outline for carousel buttons

--- resources/views/front/partials/courses.blade.php
+++ resources/views/front/partials/courses.blade.php
@@ -5,7 +5,7 @@
     <div class="row course-row">
         @foreach($courses as $index => $course)
             <div class="col-md-4 col-sm-4 course-box">
-                <h4 class="item-name">{{ $course->name }}</h4>
+                <a href="/courses/{{ $course->category->slug }}/#{{ $course->slug }}"><h4 class="item-name">{{ $course->name }}</h4></a>
                 <p class="item-description">{{ $course->aim }}</p>
             </div>
             @if($index === 2)
