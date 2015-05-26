ea075c1b38c34655b42c8c0fe07330d4b63e033c
Michael Joyner <joyner.michael@gmail.com>
2015-05-26 10:52:56 +0800

    google webmaster verification

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="RBGj5LGkW2f6nCkcs__WIh2J_nSiIeo6FX5OgY7Yk6s" />
 	@section('seo')
 	<title>Isomark</title>
 	@show
